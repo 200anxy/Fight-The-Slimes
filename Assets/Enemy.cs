5a3fcd51eb12878edcf333145fdfd477707bdc69
200anxy <aadarshnair.p@gmail.com>
2024-04-11 19:21:14 +1000

    Add resources and update Enemy.cs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+//importing unityengine's UI
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject Player;
+    public float speed;
+    private float distance;
     public Animator animator;
     public float Health{
 
@@ -19,17 +24,35 @@ public class Enemy : MonoBehaviour
             return health;
         }
     }
-         
+    
    public CapsuleCollider2D CapsuleColliderforSlime; 
     public bool damaged = false;
 
     public float health = 10;
+    public float maxHealth;
+    public Slider healthBar;
     public void Start(){
         CapsuleColliderforSlime = GetComponent<CapsuleCollider2D>(); 
+        maxHealth = health;
+        Player = GameObject.Find("Player");
+    }
+    public void Update(){
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+
+
+    }
+    private void Awake()
+    {
+        healthBar.maxValue = health;
+        healthBar.value = health;
+
     }
     public void Damaged(){
         if (damaged){
         animator.SetTrigger("Damaged");
+        healthBar.value = health;
         damaged = false;
         }else{
             print("NONE"); //This is just for testing purposes
@@ -43,4 +66,6 @@ public class Enemy : MonoBehaviour
     public void RemoveEnemy(){
         Destroy(gameObject);
     }
+
+
 }
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Thanks and also pls leave a
 If you think there any improvements to be made, let me know my doing a pull request or smthn like that
 
 # USE THE CODE IF YOU WANT TO, its from the yt video
+ALL RESOURCES USED FOR THIS PROJECT ARE FREE AND ARE LISTED BELOW
+1. https://www.youtube.com/watch?v=7iYWpzL9GkM
+2. [Mystic-Woods Asset Pack](https://game-endeavor.itch.io/mystic-woods)
+3. 
